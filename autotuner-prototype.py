68ad15fd06dcb046cd979d5776fd5f882b1ba9ce
vsSchweitzer <schweitzer.vinicius@gmail.com>
2017-08-31 17:00:54 -0300

    Adicionadas flags

--- autotuner-prototype.py
+++ autotuner-prototype.py
@@ -12,12 +12,19 @@ def tuner(argv):
     exec_file = 'matmult'
     compilation_line = ['gcc','-o',exec_file,'mm.c']
     steps = ['-DSTEP=']
-    possible_steps = range(1, 6)
-    possible_flags = ['-O2', '-O3', '-Ofast', '-funroll-all-loops', '-funroll-loops']
+    possible_steps = range(1, 4)
+    possible_flags = ['-O2', '-O3', '-Ofast', '-funroll-all-loops', '-funroll-loops', '-fno-defer-pop',
+                      '-fforce-addr', '-fomit-frame-pointer', '-finline-functions',
+                      '-ffast-math', '-fstrength-reduce', '-fthread-jumps']
     current_flags = []
     historico_tempo = {}
 
-    for i in range(1, len(possible_flags) + 1):
+    combination_depth = int(argv[0])  # A quantidade máxima de flags por combinação
+    isDebug = ('-d' in argv)
+
+    print("Iniciando autotuning")
+
+    for i in range(1, combination_depth + 1):
 
         flags_combinadas = itertools.combinations(possible_flags, i)
 
@@ -28,26 +35,32 @@ def tuner(argv):
             for step in possible_steps:
                 step_power = pow(2, step)
                 current_step = [steps[0] + str(step_power)]
-                #print(compilation_line + current_step + current_flags)
+
+                if isDebug:
+                	print(compilation_line + current_step + current_flags)
 
                 # Compile code
                 compilation_try = subprocess.run(compilation_line + current_step + current_flags)
-                if (compilation_try.returncode == 0):
-                    print("Happy compilation")
-                else:
+                if (compilation_try.returncode != 0):
                     print("Sad compilation")
+                else:
+                    if isDebug:
+                        print("Happy compilation")
 
                 # Run code
                 input_size = str(4)
-                print("Start execution\n")
+                if isDebug:
+                    print("Start execution\n")
                 t_begin = time.time() # timed run
                 run_trial = subprocess.run(['./'+exec_file, input_size])
                 t_end = time.time()
                 tempo_decorrido = t_end-t_begin
-                if (run_trial.returncode == 0):
-                    print("\nHappy execution in "+str(tempo_decorrido))
-                else:
+                if (run_trial.returncode != 0):
                     print("\nSad execution")
+                else:
+                    if isDebug:
+                        print("\nHappy execution in "+str(tempo_decorrido))
+                    
                 if tempo_decorrido < tempo_best_step:
                     tempo_best_step = tempo_decorrido
             historico_tempo[tuple(current_step + current_flags)] = tempo_best_step
